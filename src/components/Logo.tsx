e58eeedd061c109bd51f05d8fdcacfaa059e3fca
Arshad Khan <Arshad.az@outlook.com>
2026-09-16 10:55:09 +0530

    brand: AVEDIKA polygon logo mark in navbar + SVG favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,3 @@
 export default function Logo() {
-  return (
-    <span className="inline-flex items-center justify-center border-4 border-black bg-neo-secondary p-1">
-      <svg width="30" height="30" viewBox="0 0 256 256" aria-label="AVEDIKA AI logo" className="block">
-        <path d="M 208 52 L 208 208 L 48 208 Z" fill="black" />
-      </svg>
-    </span>
-  )
+  return <img src="/favicon.svg" width={30} height={30} alt="AVEDIKA AI logo" className="block" />
 }
